fbc4699fd9678f61ba59370bba86824846ba1744
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-07-17 16:55:37 +0600

    navbar setup with initial structure

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-4xl text-center font-bold">This is home page.</h1>
+        </div>
+    );
+};
+
+export default Home;
